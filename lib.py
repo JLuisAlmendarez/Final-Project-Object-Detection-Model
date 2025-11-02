5aafa82d55c6ea2e16904c24d287e1326bca627a
José Luis Almendarez González <almendarez1002@gmail.com>
2025-11-02 07:24:44 -0600

    Ignorar archivos de sistema

---
+++ lib.py
@@ -0,0 +1,9 @@
+import os
+from collections import defaultdict
+import json
+import shutil
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from pycocotools.coco import COCO
+import random
+import ipywidgets as widgets
